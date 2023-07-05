ebc2d2cf1992dcb863537c45b83e08345fb17d97
DungNguyen1999 <dungnguyen2342@gmail.com>
2023-07-05 21:12:04 +0700

    fix UI, update List page

--- pages/Detail/Detail.js
+++ pages/Detail/Detail.js
@@ -1,33 +1,33 @@
 // SHOW IMG
 
 function showImage(src) {
-  const fullscreenImage = document.getElementById("fullscreen-image");
-  const fullscreenImageSrc = document.getElementById("fullscreen-image-src");
+   const fullscreenImage = document.getElementById('fullscreen-image');
+   const fullscreenImageSrc = document.getElementById('fullscreen-image-src');
 
-  fullscreenImageSrc.src = src;
-  fullscreenImage.classList.add("show");
+   fullscreenImageSrc.src = src;
+   fullscreenImage.classList.add('show');
 }
 
 function closeFullscreenImage() {
-  const fullscreenImage = document.getElementById("fullscreen-image");
+   const fullscreenImage = document.getElementById('fullscreen-image');
 
-  fullscreenImage.classList.remove("show");
+   fullscreenImage.classList.remove('show');
 }
 
 // QUANTITY PRICE
 
-let minusButton = document.querySelector(".minus-btn");
-let plusButton = document.querySelector(".plus-btn");
+let minusButton = document.querySelector('.minus-btn');
+let plusButton = document.querySelector('.plus-btn');
 let quantityInput = document.querySelector("input[name='quantity']");
 
-minusButton.addEventListener("click", function () {
-  const currentValue = parseInt(quantityInput.value);
-  if (currentValue > 1) {
-    quantityInput.value = currentValue - 1;
-  }
+minusButton.addEventListener('click', function () {
+   const currentValue = parseInt(quantityInput.value);
+   if (currentValue > 1) {
+      quantityInput.value = currentValue - 1;
+   }
 });
 
-plusButton.addEventListener("click", function () {
-  const currentValue = parseInt(quantityInput.value);
-  quantityInput.value = currentValue + 1;
+plusButton.addEventListener('click', function () {
+   const currentValue = parseInt(quantityInput.value);
+   quantityInput.value = currentValue + 1;
 });
